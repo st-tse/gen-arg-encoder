13f2d6895c1cd595d57427554809c3458709f2ce
Stephen <st624@cornell.edu>
2022-05-04 19:24:10 -0400

    added all dataloaders

--- bert_train.py
+++ bert_train.py
@@ -11,12 +11,24 @@ from src.genie.data import IEDataset, my_collate #temp remove .
 tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
 model = BertForMaskedLM.from_pretrained('bert-base-uncased')
 
-dataset = IEDataset('preprocessed_rams/train_RAMS_oracle.jsonl')
+train = IEDataset('preprocessed_rams/train_RAMS_oracle.jsonl')
+test = IEDataset('preprocessed_rams/test_RAMS_oracle.jsonl')
+val = IEDataset('preprocessed_rams/val_RAMS_oracle.jsonl')
         
-dataloader = DataLoader(dataset, 
+train = DataLoader(train, 
     pin_memory=True, num_workers=2, 
     collate_fn=my_collate,
     batch_size=16, 
     shuffle=True)
 
-optimizer = AdamW()
+test = DataLoader(test, 
+    pin_memory=True, num_workers=2, 
+    collate_fn=my_collate,
+    batch_size=16, 
+    shuffle=True)
+
+val = DataLoader(val, 
+    pin_memory=True, num_workers=2, 
+    collate_fn=my_collate,
+    batch_size=16, 
+    shuffle=True)
--- src/genie/bert_train.py
+++ src/genie/bert_train.py
@@ -1,1 +1,34 @@
-from transformers import TFAutoModelForMaskedLM
+import torch
+from transformers import BertTokenizer, BertForMaskedLM
+
+import argparse
+from src.genie.data_module import RAMSDataModule
+
+from torch.utils.data import DataLoader 
+
+from src.genie.data import IEDataset, my_collate #temp remove .
+
+tokenizer = BertTokenizer.from_pretrained('bert-base-uncased')
+model = BertForMaskedLM.from_pretrained('bert-base-uncased')
+
+train = IEDataset('preprocessed_rams/train_RAMS_oracle.jsonl')
+test = IEDataset('preprocessed_rams/test_RAMS_oracle.jsonl')
+val = IEDataset('preprocessed_rams/val_RAMS_oracle.jsonl')
+        
+train_dataloader = DataLoader(train, 
+    pin_memory=True, num_workers=2, 
+    collate_fn=my_collate,
+    batch_size=16, 
+    shuffle=True)
+train_dataloader = DataLoader(test, 
+    pin_memory=True, num_workers=2, 
+    collate_fn=my_collate,
+    batch_size=16, 
+    shuffle=True)
+val_dataloader = DataLoader(val, 
+    pin_memory=True, num_workers=2, 
+    collate_fn=my_collate,
+    batch_size=16, 
+    shuffle=True)
+
+
